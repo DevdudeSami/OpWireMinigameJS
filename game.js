3e803d734cb6598f2db6d1d3155c40d27ef7ae73
Youssef Moawad <youssef_programming@icloud.com>
2018-01-29 23:03:38 +0000

    Can now click lights and they light up.

--- game.js
+++ game.js
@@ -2,12 +2,31 @@ var game;
 
 $(function() {
   game = $("#game");
-
   showSpriteNumber(0);
+
+  $(".light").mousedown(lightDown);
+  $(".light").mouseup(lightUp);
 })
 
 function showSpriteNumber(number) {
   var offset = number*900;
 
   game.css("background-position", `-${offset}px, 0px`);
+}
+
+function lightDown() {
+  var id = $(this).attr('id');
+  var lightIndex = parseInt(id.charAt(id.length - 1));
+
+  showSpriteNumber(lightIndex+8);
+}
+function lightUp() {
+  var id = $(this).attr('id');
+  var lightIndex = parseInt(id.charAt(id.length - 1));
+
+  showSpriteNumber(0);
+}
+
+function turnOnLight(lightIndex) {
+
 }
